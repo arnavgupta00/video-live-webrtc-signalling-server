38fce03340183b48378eefcc4be95bcfe4a0b4b3
Arnav Gupta <101815033+arnavgupta00@users.noreply.github.com>
2024-01-29 21:33:39 +0530

    try 2 to solve error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 
 const app = express();
 const server = http.createServer(app);
@@ -23,48 +23,43 @@ var roomList = [
   "roomRoom10",
 ];
 
-
-
-
-wss.on('connection', (ws) => {
-
-  ws.on('message', (message) => {
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
     const data = JSON.parse(message);
     switch (data.type) {
-      case 'joinRoom':
+      case "joinRoom":
         handleJoinRoom(ws, data);
         break;
-      case 'createRoom':
+      case "createRoom":
         handleCreateRoom(ws, data);
         break;
-      case 'offer':
+      case "offer":
         handleOffer(ws, data);
         break;
-      case 'answer':
+      case "answer":
         handleAnswer(ws, data);
         break;
-      case 'iceCandidate':
+      case "iceCandidate":
         handleIceCandidate(ws, data);
         break;
-      case 'disconnect':
+      case "disconnect":
         handleDisconnect(ws);
         break;
-      case 'clientList':
+      case "clientList":
         handleClientList(ws);
         break;
       case "chat":
         handleChat(ws, data);
-        break
+        break;
       default:
         console.log(message);
     }
   });
-  ws.on('close', (code, reason) => {
-    console.log("close initiated")
+  ws.on("close", (code, reason) => {
+    console.log("close initiated");
     try {
-      handleDisconnect(ws)
-    }
-    catch (err) {
+      handleDisconnect(ws);
+    } catch (err) {
       console.log(err);
     }
   });
@@ -76,20 +71,22 @@ const generateClientId = () => {
 
 const handleJoinRoom = (ws, data) => {
   const { roomId, userId } = data;
-  
+
   // add the client to the room
-  if (!rooms[roomId] ) {
-    console.log(roomId)
-    ws.send(JSON.stringify({
-      type: 'error',
-      payload: "roomDNE",
-    }));
-  }else if(roomList.includes(roomId)){
-    console.log(roomId)+" is in roomList"
+  if (roomList.includes(roomId)) {
+    console.log(roomId) + " is in roomList";
     if (!rooms[roomId]) {
       rooms[roomId] = [];
-      rooomClients[roomId] = []
+      rooomClients[roomId] = [];
     }
+  } else if (!rooms[roomId]) {
+    console.log(roomId);
+    ws.send(
+      JSON.stringify({
+        type: "error",
+        payload: "roomDNE",
+      })
+    );
   } else {
     const clientId = generateClientId();
     ws.roomId = roomId;
@@ -103,37 +100,41 @@ const handleJoinRoom = (ws, data) => {
       rooomClients[roomId].push(user.id);
       const client = clientsArray.find((client) => client.id === user.id);
       if (client && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({
-          type: 'userJoined',
-          payload: { userId, socketId: clientId, allUsers: rooms[roomId] },
-        }));
+        client.send(
+          JSON.stringify({
+            type: "userJoined",
+            payload: { userId, socketId: clientId, allUsers: rooms[roomId] },
+          })
+        );
       }
     });
     //
     // notify the new user about existing users
 
-    const listCPY = rooomClients[roomId].filter(id => id !== ws.id);
+    const listCPY = rooomClients[roomId].filter((id) => id !== ws.id);
 
-    ws.send(JSON.stringify({
-      type: 'clientList',
-      list: listCPY,
-    }));
-    ws.send(JSON.stringify({
-      type: 'initialClientList',
-      list: listCPY,
-    }));
-    ws.send(JSON.stringify({
-      type: 'allUsers',
-      payload: rooms[roomId],
-    }));
+    ws.send(
+      JSON.stringify({
+        type: "clientList",
+        list: listCPY,
+      })
+    );
+    ws.send(
+      JSON.stringify({
+        type: "initialClientList",
+        list: listCPY,
+      })
+    );
+    ws.send(
+      JSON.stringify({
+        type: "allUsers",
+        payload: rooms[roomId],
+      })
+    );
   }
-
-
-
 };
 
 const handleClientList = (ws) => {
-
   var roomId = ws.roomId;
   const clientsArray = Array.from(wss.clients);
 
@@ -141,22 +142,23 @@ const handleClientList = (ws) => {
     rooomClients[roomId].push(user.id);
   });
 
-  var listClientsSet = new Set(rooomClients[roomId])
+  var listClientsSet = new Set(rooomClients[roomId]);
   var listClients = Array.from(listClientsSet);
 
-  const listCPY = listClients.filter(id => id !== ws.id);
+  const listCPY = listClients.filter((id) => id !== ws.id);
 
   rooms[roomId].forEach((user) => {
-    
     const client = clientsArray.find((client) => client.id === user.id);
     if (client && client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({
-        type: 'clientList',
-        list: listCPY,
-      }));
+      client.send(
+        JSON.stringify({
+          type: "clientList",
+          list: listCPY,
+        })
+      );
     }
   });
-}
+};
 
 const handleCreateRoom = (ws, data) => {
   const { roomId, userId } = data;
@@ -164,7 +166,7 @@ const handleCreateRoom = (ws, data) => {
   // add the client to the room
   if (!rooms[roomId]) {
     rooms[roomId] = [];
-    rooomClients[roomId] = []
+    rooomClients[roomId] = [];
   }
   const clientId = generateClientId();
   ws.roomId = roomId;
@@ -174,73 +176,66 @@ const handleCreateRoom = (ws, data) => {
   rooomClients[roomId].push(ws.id);
   console.log(rooms[roomId]);
 
-
-
-
   // notify the new user about existing users
-  ws.send(JSON.stringify({
-    type: 'allUsers',
-    payload: rooms[roomId],
-  }));
-
-
+  ws.send(
+    JSON.stringify({
+      type: "allUsers",
+      payload: rooms[roomId],
+    })
+  );
 };
 
-
 const handleOffer = (ws, data) => {
-
-  
-
   const clientsArray = Array.from(wss.clients);
   rooms[ws.roomId].forEach((user) => {
     const client = clientsArray.find((client) => client.id === user.id);
     if (client.id === data.target && client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({
-        type: 'offer',
-        payloadOffer: data.offer,
-        senderID: ws.id, //we send ws.id here so the id created in frontend is useless in backend
-        negotiation: data.negotiation,
-      }));
-
-      
+      client.send(
+        JSON.stringify({
+          type: "offer",
+          payloadOffer: data.offer,
+          senderID: ws.id, //we send ws.id here so the id created in frontend is useless in backend
+          negotiation: data.negotiation,
+        })
+      );
     }
   });
 };
 
 const handleAnswer = (ws, data) => {
-  
   const clientsArray = Array.from(wss.clients);
   rooms[ws.roomId].forEach((user) => {
     const client = clientsArray.find((client) => client.id === user.id);
     if (client.id === data.target && client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({
-        type: 'answer',
-        answer: data.answer,
-        senderID: ws.id,
-        negotiation: data.negotiation,
-      }));
-      console.log(`ANSWER SENT FROM ${ws.id} TO ${data.target}`)
-      
+      client.send(
+        JSON.stringify({
+          type: "answer",
+          answer: data.answer,
+          senderID: ws.id,
+          negotiation: data.negotiation,
+        })
+      );
+      console.log(`ANSWER SENT FROM ${ws.id} TO ${data.target}`);
     }
-
   });
 };
 
 const handleIceCandidate = (ws, data) => {
   const clientsArray = Array.from(wss.clients);
-  
+
   rooms[ws.roomId].forEach((user) => {
     const client = clientsArray.find((client) => client.id === user.id);
     if (client.id === data.target && client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({
-        type: 'candidate',
-        candidate: data.candidate,
-        senderID: ws.id,
-      }));
-      console.log(`ICE CANDIDATE SENT FROM ${ws.id} TO ${client.id}`, data)
+      client.send(
+        JSON.stringify({
+          type: "candidate",
+          candidate: data.candidate,
+          senderID: ws.id,
+        })
+      );
+      console.log(`ICE CANDIDATE SENT FROM ${ws.id} TO ${client.id}`, data);
     }
   });
-
 };
 
 const handleChat = (ws, data) => {
@@ -248,20 +243,20 @@ const handleChat = (ws, data) => {
   rooms[ws.roomId].forEach((user) => {
     const client = clientsArray.find((client) => client.id === user.id);
     if (client.id !== ws.id && client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({
-        type: 'chat',
-        message: data.message,
-        senderID: ws.id,
-        senderName: data.senderName,
-
-      }));
-      console.log(`CHAT SENT FROM ${ws.id} TO ${client.id}`, data)
+      client.send(
+        JSON.stringify({
+          type: "chat",
+          message: data.message,
+          senderID: ws.id,
+          senderName: data.senderName,
+        })
+      );
+      console.log(`CHAT SENT FROM ${ws.id} TO ${client.id}`, data);
     }
   });
-}
+};
 
 const handleDisconnect = (ws) => {
-
   roomid = ws.roomId;
 
   if (rooms[roomid]) {
@@ -271,8 +266,6 @@ const handleDisconnect = (ws) => {
     }
   }
 };
-
-
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
